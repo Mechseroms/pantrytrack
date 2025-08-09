bac94274572bcb502ebec77f33ad7320bce25da8
Jadowyne Ulve <mechserom@gmail.com>
2025-08-09 17:30:34 -0500

    implemented darkmode into site_management module also fixed the navbar

--- application/site_management/static/js/site_management_handler.js
+++ application/site_management/static/js/site_management_handler.js
@@ -23,26 +23,6 @@ async function changeSite(site){
     location.reload(true)
 }
 
-var mode = false
-async function toggleDarkMode() {
-    let darkMode = document.getElementById("dark-mode");    
-    darkMode.disabled = !darkMode.disabled;
-    mode = !mode;
-    if(mode){
-        document.getElementById('modeToggle').innerHTML = "light_mode"
-        document.getElementById('main_html').classList.add('uk-light')
-        
-    } else {
-        document.getElementById('modeToggle').innerHTML = "dark_mode"
-        document.getElementById('main_html').classList.remove('uk-light')
-    }
-}
-
-if(session.user.flags.darkmode){
-    toggleDarkMode()
-}
-
-
 document.addEventListener('DOMContentLoaded', async function() {
     let zones = await fetchZones()
     await updateZonesPagination()
